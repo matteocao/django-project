5daaa0c9509a00700cd5c88e0fc3a406f5f83793
giotto-learn <maintainers@giotto.ai>
2021-01-26 15:25:40 +0100

    back to defaults for rq

--- gtdainterface/settings.py
+++ gtdainterface/settings.py
@@ -154,10 +154,7 @@ RQ_QUEUES = {
         },
     },
     'high': {
-        'HOST': 'localhost',
-        'PORT': 6379,
-        'URL': os.getenv('REDISTOGO_URL','redis://localhost:6379'), # If you're on Heroku
-        'DB': 0,
+        'URL': os.getenv('REDISTOGO_URL','redis://localhost:6379/0'), # If you're on Heroku
         'DEFAULT_TIMEOUT': 500,
     },
     'low': {
